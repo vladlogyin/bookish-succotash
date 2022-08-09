bc68a57820dbeb19cbabc566dfa3990ed37de838
Vlad Logyin <vladlogyin@gmail.com>
2022-08-09 09:46:39 +0100

    Add a TestController (not meant to be merged)

--- src/main/java/com/sparta/spartafinalproject/controllers/TestController.java
+++ src/main/java/com/sparta/spartafinalproject/controllers/TestController.java
@@ -1,7 +1,9 @@
 package com.sparta.spartafinalproject.controllers;
 
 import com.sparta.spartafinalproject.documents.Comment;
+import com.sparta.spartafinalproject.documents.Movie;
 import com.sparta.spartafinalproject.repositories.CommentRepository;
+import com.sparta.spartafinalproject.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +15,28 @@ public class TestController {
     @Autowired
     private CommentRepository repo;
 
+    @Autowired
+    private MovieRepository repo2;
+
     @GetMapping("/comments")
     public List<Comment> getComments()
     {
         return repo.findAll();
     }
+    @GetMapping("/comment/by-id")
+    public Comment getComment()
+    {
+        return repo.findById("5a9427648b0beebeb6957a22").get();
+    }
     @GetMapping("/commentcount")
     public long getCommentCount()
     {
         return repo.count();
+    }
+
+    @GetMapping("/movies")
+    public List<Movie> getMovies()
+    {
+        return repo2.findAll();
     }
 }
