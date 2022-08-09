664aa3ef8a86c4da3443b9bb9648c06b5ae92ec3
Vlad Logyin <vladlogyin@gmail.com>
2022-08-09 13:24:06 +0100

    Fix theaterId field

--- src/main/java/com/sparta/spartafinalproject/documents/Theater.java
+++ src/main/java/com/sparta/spartafinalproject/documents/Theater.java
@@ -3,12 +3,14 @@ package com.sparta.spartafinalproject.documents;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document(collection="theaters")
 public class Theater {
     @Id
     private String id;
-    private int theaterid;
+    @Field("theaterId")
+    private int theaterId;
     private Location location;
 
 }
