d5e8e74acb14ebb823601c22d38187c59e7693f0
Vlad Logyin <vladlogyin@gmail.com>
2022-08-09 11:15:30 +0100

    Add comment query methods: findAllByMovie, findAllByEmail

--- src/main/java/com/sparta/spartafinalproject/repositories/CommentRepository.java
+++ src/main/java/com/sparta/spartafinalproject/repositories/CommentRepository.java
@@ -1,11 +1,17 @@
 package com.sparta.spartafinalproject.repositories;
 
 import com.sparta.spartafinalproject.documents.Comment;
+import com.sparta.spartafinalproject.documents.Movie;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CommentRepository extends MongoRepository<Comment, String> {
 
+    List<Comment> findAllByEmail(String email);
+
+    List<Comment> findAllByMovie(Movie movie);
 
 }
