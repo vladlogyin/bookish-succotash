bb69c62cdcea6e1e31ae08eeb3c2bc3f3da2061c
Vlad Logyin <vladlogyin@gmail.com>
2022-08-09 09:37:04 +0100

    Get mongodb to work

--- src/main/java/com/sparta/spartafinalproject/repositories/UserRepository.java
+++ src/main/java/com/sparta/spartafinalproject/repositories/UserRepository.java
@@ -1,4 +1,10 @@
 package com.sparta.spartafinalproject.repositories;
 
-public interface UserRepository {
+import com.sparta.spartafinalproject.documents.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository  extends MongoRepository<User, String> {
+
 }
