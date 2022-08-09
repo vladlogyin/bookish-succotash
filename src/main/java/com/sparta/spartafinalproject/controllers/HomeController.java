ced083ea44dbbb9f6a5ac56d55309f7f3b0dc034
M-Matson <108122866+M-Matson@users.noreply.github.com>
2022-08-09 16:04:35 +0100

    Homepage + Controller, changes to login and logout html pages

---
+++ src/main/java/com/sparta/spartafinalproject/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package com.sparta.spartafinalproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String displayHomepage(){
+        return "homepage";
+    }
+    @GetMapping("/login")
+    public String logInForm(){
+        return "security/login";
+    }
+    @GetMapping("/logout")
+    public String loggingOut(){
+        return "security/logout";
+    }
+}
