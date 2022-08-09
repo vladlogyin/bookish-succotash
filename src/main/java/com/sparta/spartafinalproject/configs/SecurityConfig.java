1075860575d87dc54ab402b6d24bbe84df3af610
M-Matson <108122866+M-Matson@users.noreply.github.com>
2022-08-09 16:12:45 +0100

    returned SecurityConfig.java to normal

--- src/main/java/com/sparta/spartafinalproject/configs/SecurityConfig.java
+++ src/main/java/com/sparta/spartafinalproject/configs/SecurityConfig.java
@@ -12,6 +12,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers
     }
 }
