0ee5f603d578b4f3f260d2a7be45fce1edad1ce3
JamieScofield <jamies_1048@outlook.com>
2022-08-10 14:01:27 +0100

    added exception handling for the movie get mapping functions

--- src/main/java/com/sparta/spartafinalproject/controllers/MovieController.java
+++ src/main/java/com/sparta/spartafinalproject/controllers/MovieController.java
@@ -3,6 +3,8 @@ package com.sparta.spartafinalproject.controllers;
 import com.sparta.spartafinalproject.documents.Movie;
 import com.sparta.spartafinalproject.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,13 +21,18 @@ public class MovieController {
     }
 
     @GetMapping("/movies/id/{id}")
-    public Movie getMovieById(@PathVariable String id){
-        return repo.findById(id).get();
+    public ResponseEntity<Movie> getMovieById(@PathVariable String id){
+        if(repo.existsById(id)){
+            return ResponseEntity.status(HttpStatus.OK).body(repo.findById(id).get());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
-
     @GetMapping("/movies/title/{title}")
-    public List<Movie> getMovieByTitle(@PathVariable String title){
-        return repo.findByTitle(title);
+    public ResponseEntity<List<Movie>> getMovieByTitle(@PathVariable String title){
+        if(repo.existsByTitle(title)){
+            return ResponseEntity.status(HttpStatus.OK).body(repo.findByTitle(title));
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
 
     @PostMapping("/movies/add")
